44cce3971636cd0124a12911ac771bcbf87cad54
kanakweb <kanakwebdev@gmail.com>
2022-03-26 01:54:45 +0600

    catch details item

--- src/Components/Fruit/Fruit.js
+++ src/Components/Fruit/Fruit.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Fruit.css'
+
+
 const Fruit = (props) => {
-    const{fruit}=props;
+
+    const{handleAddToCart,fruit}=props
     const{picture,name,price}=fruit
     
+   
+   
     return (
-        
-           
-  <div className="col-sm-12 col-md-6 col-lg-4">
-    <div className="card h-100">
-      <img src={picture} className="card-img-top img-sizeing" alt=""/>
+   
+  <div className="col-sm-12 col-md-6 col-lg-4 ">
+    <div className="card card-sizing ">
+      <img src={picture} className="card-img-top img-sizeing " alt=""/>
       <div className="card-body">
         <h2 className="card-title mt-5">{name}</h2>
         <h5 className='mt-4'>Price:$ {price}</h5>
       </div>
       
-       <div><button className='btn w-100 btn-warning'>ADD To CART</button></div>
+       <button  onClick={()=>handleAddToCart(fruit)}  className='btn w-100 btn-warning button-size'>ADD To CART</button>
      
     </div>
   </div>
--- src/Components/Fruits/Fruits.js
+++ src/Components/Fruits/Fruits.js
@@ -5,25 +5,40 @@ import Fruit from '../Fruit/Fruit';
 import './Fruits.css'
 
 const Fruits = () => {
+
     const[fruits,setFruits]=useState([])
+    const[carts,setCarts]=useState([])
     useEffect(()=>{
         fetch('data.json')
         .then(res=>res.json())
         .then(data=>setFruits(data))
     },[])
-    return (
 
-           <div className='sizeing'>
+    const handleAddToCart=(fruit)=>{
+       const newCart=[...carts,fruit]
+       setCarts(newCart);
+    }
+    return (
+      /* details item */
+           <div className='sizeing py-4'>
                 <div className='row g-4'>
                  <div className='col-12 col-lg-3 color'>
             <h1>Details</h1>
+            {
+                carts.map(cart=><div className='bg-light m-2 p-2'>
+                    {cart.name}
+                    </div>)
+            }
                 </div>
+
+                {/* fruit items and cards */}
                 <div className='col-sm-12  col-lg-9'>
                 <div className="row g-4">
                 {
                     fruits.map(fruit=><Fruit 
                         key={fruit.id}
                         fruit={fruit}
+                       handleAddToCart={handleAddToCart}
                     ></Fruit>)
                 }
             </div>
